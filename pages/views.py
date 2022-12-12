a4950468eced68531dd0ec3e594b831a0587847f
FrostyFeet7 <tyzer.clark@umconnect.umt.edu>
2022-12-11 21:43:32 -0700

    model file udated. fixing issues with linking to model rules

--- pages/views.py
+++ pages/views.py
@@ -5,5 +5,5 @@ class HomePageView(TemplateView):
     template_name = "home.html"
 
 class MonthlyCalenderView(TemplateView):
-    template_name = "calenderView/fullcalendar.html"
+    template_name = "fullcalendar.html"
 # Create your views here.
